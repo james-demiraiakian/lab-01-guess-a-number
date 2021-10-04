a3be00fe8d8beb7b61dd837c5309e7aa702e3ec1
James Demiraiakian <james.armen@gmail.com>
2021-10-04 15:46:43 -0700

    event listener partially working. Detects i too high/low, fails on === and counter = 0

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-//import { random } from "./random.js";
-
-const userGuess = document.getElementById('user-guess');
+const userGuess = document.getElementById('user-guess').value;
 const guessBut = document.getElementById('guess-but');
+const result = document.getElementById('result');
 const reset = document.getElementById('reset');
+let trys = document.getElementById('num-guess');
 const randNum = (Math.floor(Math.random() * 20)) + 1;
-// initialize global state
-let numGuess = 4
-let result;
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
+
+trys = 4;
+let numGuess = 4;
+let guessResult;
+
+
 
   guessBut.addEventListener('click', ()=> {
     numGuess--;
-    
     if (userGuess === randNum) {
-      result = 'Correct!';
+      guessResult = 'Correct!';
+    } else 
+    if (numGuess = 0) {
+      guessResult = 'Out of Guesses!';
     } else
-
     if (userGuess < randNum) {
-      result = 'Too Low!';
-    } else
-    
+      guessResult = 'Too Low!';
+    } else 
     if (userGuess > randNum) {
-      result = 'Too High!'
+      guessResult = 'Too High!';
     }
 
-    console.log(numGuess);
+    result.textContent = guessResult;
   });
 
   reset.addEventListener('click', () => {
     numGuess = 4;
+    guessResult = ' ';
   });
