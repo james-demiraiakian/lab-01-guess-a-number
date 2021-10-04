fbc4363dc68899589601bffd0cdedd4b0564c86d
James Demiraiakian <james.armen@gmail.com>
2021-10-04 15:07:20 -0700

    HTML Skeleton, minor CSS, partway through js event listener

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-// import functions and grab DOM elements
+//import { random } from "./random.js";
 
+const userGuess = document.getElementById('user-guess');
+const guessBut = document.getElementById('guess-but');
+const reset = document.getElementById('reset');
+const randNum = (Math.floor(Math.random() * 20)) + 1;
 // initialize global state
-
+let numGuess = 4
+let result;
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+  guessBut.addEventListener('click', ()=> {
+    numGuess--;
+    
+    if (userGuess === randNum) {
+      result = 'Correct!';
+    } else
+
+    if (userGuess < randNum) {
+      result = 'Too Low!';
+    } else
+    
+    if (userGuess > randNum) {
+      result = 'Too High!'
+    }
+
+    console.log(numGuess);
+  });
+
+  reset.addEventListener('click', () => {
+    numGuess = 4;
+  });
