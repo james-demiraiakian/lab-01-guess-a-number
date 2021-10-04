1fd4cb02b7a1ddd71b326273e9c71545a4b9a0cf
James Demiraiakian <james.armen@gmail.com>
2021-10-04 16:32:02 -0700

    base HTML/JS working for guessing and disabling. Working on CSS and stretch

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-const userGuess = document.getElementById('user-guess').value;
+const userGuess = document.getElementById('user-guess');
 const guessBut = document.getElementById('guess-but');
 const result = document.getElementById('result');
 const reset = document.getElementById('reset');
-let trys = document.getElementById('num-guess');
-const randNum = (Math.floor(Math.random() * 20)) + 1;
+let trys = document.getElementById('trys');
+let randNum = (Math.floor(Math.random() * 20)) + 1;
 
-
-trys = 4;
 let numGuess = 4;
 let guessResult;
 
-
+console.log(randNum);
 
   guessBut.addEventListener('click', ()=> {
     numGuess--;
-    if (userGuess === randNum) {
+    
+    console.log(userGuess.value);
+
+    const userGuessVal = Number(userGuess.value);
+
+    if (userGuessVal === randNum) {
       guessResult = 'Correct!';
+      guessBut.disabled = true;
     } else 
-    if (numGuess = 0) {
+    if (numGuess === 0) {
       guessResult = 'Out of Guesses!';
+      guessBut.disabled = true;
     } else
-    if (userGuess < randNum) {
+    if (userGuessVal < randNum) {
       guessResult = 'Too Low!';
     } else 
-    if (userGuess > randNum) {
+    if (userGuessVal > randNum) {
       guessResult = 'Too High!';
     }
 
+    trys.textContent = `${numGuess} trys remain.`;
     result.textContent = guessResult;
   });
 
   reset.addEventListener('click', () => {
     numGuess = 4;
-    guessResult = ' ';
+    guessBut.disabled = false;
+    trys.textContent = '4 trys remain.';
   });
