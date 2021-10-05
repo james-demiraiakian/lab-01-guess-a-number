603a9e24eba037f28950845638aa07105bac167e
James Demiraiakian <james.armen@gmail.com>
2021-10-05 11:03:53 -0700

    fix eslint errors

--- app.js
+++ app.js
@@ -3,43 +3,42 @@ const guessBut = document.getElementById('guess-but');
 const result = document.getElementById('result');
 const reset = document.getElementById('reset');
 let trys = document.getElementById('trys');
-let numCorrect = document.getElementById('correct');
+//let numCorrect = document.getElementById('correct');
 let randNum = (Math.floor(Math.random() * 20)) + 1;
 
 let numGuess = 4;
 let guessResult;
-
+//numCorrect = 0;
 //console.log('Initial Random Number is', randNum);
-
-  guessBut.addEventListener('click', ()=> {
+guessBut.addEventListener('click', ()=> {
     numGuess--;
 
     const userGuessVal = Number(userGuess.value);
 
     if (userGuessVal === randNum) {
-      guessResult = 'Correct!';
-      guessBut.disabled = true;
+        guessResult = 'Correct!';
+        guessBut.disabled = true;
     } else 
     if (numGuess === 0) {
-      guessResult = 'Out of Guesses!';
-      guessBut.disabled = true;
+        guessResult = 'Out of Guesses!';
+        guessBut.disabled = true;
     } else
     if (userGuessVal < randNum) {
-      guessResult = 'Too Low!';
+        guessResult = 'Too Low!';
     } else 
     if (userGuessVal > randNum) {
-      guessResult = 'Too High!';
+        guessResult = 'Too High!';
     }
 
     trys.textContent = `${numGuess} trys remain.`;
     result.textContent = guessResult;
-  });
+});
 
-  reset.addEventListener('click', () => {
+reset.addEventListener('click', () => {
     numGuess = 4;
     guessBut.disabled = false;
     trys.textContent = '4 trys remain.';
     randNum = (Math.floor(Math.random() * 20)) + 1;
 //    console.log('New Random Number is', randNum);
     userGuess.value = null;
-  });
+});
